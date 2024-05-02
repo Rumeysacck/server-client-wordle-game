f539ee4aac328435459bf1af761e73e611aacc5b
rumeysacck <rumeysa7cicek@gmail.com>
2024-05-02 15:50:43 +0300

    word layout

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ while hak > 0:
         else:
             cevap += "*"
     if hak==1 and cevap.upper()!=kelime.upper():
-        client_socket.send(f"Bilemediniz maalesef, Tahmin edilmesi gereken kelime: {kelime} \nBitti".encode())
+        client_socket.send(f"Bilemediniz maalesef, Tahmin edilmesi gereken kelime: {kelime}".encode())
         break
     
     if cevap.upper() == kelime.upper():
